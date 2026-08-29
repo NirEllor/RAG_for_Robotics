586891f9f4b0c8dc12678e5d21dd03eacdb36f96
Nir Ellor <nirellorwaizner@gmail.com>
2026-08-30 00:05:04 +0300

    Run evaluation jobs with CUDA enabled

--- slurm/run_evaluate.sh
+++ slurm/run_evaluate.sh
@@ -14,8 +14,10 @@ ANNOTATIONS="${ANNOTATIONS_OVERRIDE:-$PROJECT_ROOT/configs/evaluation/rlbench_re
 METHODS="${METHODS_OVERRIDE:-random pose_descriptor rgb_histogram global_color geometry_only uni3d ptv3}"
 KS="${KS_OVERRIDE:-1 2 3}"
 EVAL_MEM_GB="${EVAL_MEM_GB:-32G}"
+CUDA_MODULE="${CUDA_MODULE:-cuda/12.9}"
+CUDA_ROOT="${CUDA_ROOT:-/usr/local/nvidia/cuda/12.9}"
 
-sbatch $CPU_NODE_ARGS --mem="$EVAL_MEM_GB" -c2 --time=06:00:00 \
+sbatch $GPU_NODE_ARGS --mem="$EVAL_MEM_GB" -c2 --time=06:00:00 \
   --mail-type=END,FAIL --mail-user="$EMAIL" \
   --job-name="retr_eval" \
-  --wrap "bash -lc 'cd \"$PROJECT_ROOT\" && source \"$VENV_DIR/bin/activate\" && export PIP_CACHE_DIR=\"$PIP_CACHE_DIR\" HF_HOME=\"$HF_HOME\" TORCH_HOME=\"$TORCH_HOME\" && python3 scripts/evaluate_retrieval.py --dataset-root \"$DATASET_ROOT\" --annotations \"$ANNOTATIONS\" --methods $METHODS --ks $KS'"
+  --wrap "bash -lc 'cd \"$PROJECT_ROOT\" && source \"$VENV_DIR/bin/activate\" && module load \"$CUDA_MODULE\" && export CUDA_HOME=\"$CUDA_ROOT\" && export PATH=\"\$CUDA_HOME/bin:\$PATH\" && export LD_LIBRARY_PATH=\"\$CUDA_HOME/lib64:\$LD_LIBRARY_PATH\" && export PIP_CACHE_DIR=\"$PIP_CACHE_DIR\" HF_HOME=\"$HF_HOME\" TORCH_HOME=\"$TORCH_HOME\" && python3 scripts/evaluate_retrieval.py --dataset-root \"$DATASET_ROOT\" --annotations \"$ANNOTATIONS\" --methods $METHODS --ks $KS'"
