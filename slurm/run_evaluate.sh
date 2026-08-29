09bc02f5eb79d025840949fd7217b17d0a4368d1
Nir Ellor <nirellorwaizner@gmail.com>
2026-08-30 01:44:44 +0300

    Set CUDA arch list for retrieval evaluation

--- slurm/run_evaluate.sh
+++ slurm/run_evaluate.sh
@@ -16,8 +16,9 @@ KS="${KS_OVERRIDE:-1 2 3}"
 EVAL_MEM_GB="${EVAL_MEM_GB:-32G}"
 CUDA_MODULE="${CUDA_MODULE:-cuda/12.9}"
 CUDA_ROOT="${CUDA_ROOT:-/usr/local/nvidia/cuda/12.9}"
+TORCH_CUDA_ARCH_LIST="${TORCH_CUDA_ARCH_LIST:-7.5;8.6}"
 
 sbatch $GPU_NODE_ARGS --mem="$EVAL_MEM_GB" -c2 --time=06:00:00 \
   --mail-type=END,FAIL --mail-user="$EMAIL" \
   --job-name="retr_eval" \
-  --wrap "bash -lc 'cd \"$PROJECT_ROOT\" && source \"$VENV_DIR/bin/activate\" && module load \"$CUDA_MODULE\" && export CUDA_HOME=\"$CUDA_ROOT\" && export PATH=\"\$CUDA_HOME/bin:\$PATH\" && export LD_LIBRARY_PATH=\"\$CUDA_HOME/lib64:\$LD_LIBRARY_PATH\" && export PIP_CACHE_DIR=\"$PIP_CACHE_DIR\" HF_HOME=\"$HF_HOME\" TORCH_HOME=\"$TORCH_HOME\" TMPDIR=\"$TMP_ROOT\" TMP=\"$TMP_ROOT\" TEMP=\"$TMP_ROOT\" TORCH_EXTENSIONS_DIR=\"$TORCH_EXTENSIONS_DIR\" && python3 scripts/evaluate_retrieval.py --dataset-root \"$DATASET_ROOT\" --annotations \"$ANNOTATIONS\" --methods $METHODS --ks $KS'"
+  --wrap "bash -lc 'cd \"$PROJECT_ROOT\" && source \"$VENV_DIR/bin/activate\" && module load \"$CUDA_MODULE\" && export CUDA_HOME=\"$CUDA_ROOT\" && export PATH=\"\$CUDA_HOME/bin:\$PATH\" && export LD_LIBRARY_PATH=\"\$CUDA_HOME/lib64:\$LD_LIBRARY_PATH\" && export TORCH_CUDA_ARCH_LIST=\"$TORCH_CUDA_ARCH_LIST\" && export PIP_CACHE_DIR=\"$PIP_CACHE_DIR\" HF_HOME=\"$HF_HOME\" TORCH_HOME=\"$TORCH_HOME\" TMPDIR=\"$TMP_ROOT\" TMP=\"$TMP_ROOT\" TEMP=\"$TMP_ROOT\" TORCH_EXTENSIONS_DIR=\"$TORCH_EXTENSIONS_DIR\" && python3 scripts/evaluate_retrieval.py --dataset-root \"$DATASET_ROOT\" --annotations \"$ANNOTATIONS\" --methods $METHODS --ks $KS'"
